668d41b812fa978641e3ce237015881f2d99f6aa
Arvind <aramac13@asu.edu>
2019-11-22 00:24:36 -0700

    more bug fixes

--- CCT_graphics_page.py
+++ CCT_graphics_page.py
@@ -138,7 +138,7 @@ def plot_live_CC_data(controller, start, log):
 
 		Time_CC_temp = Split_strings[1].split('\r')[0]
 
-		Time_CC.append(datetime.strptime(Time_CC_temp, '%m/%d/%y %H::%M::%S'))
+		Time_CC.append(datetime.strptime(Time_CC_temp, '%Y-%m-%d %H:%M:%S'))
 
 		Time_in_seconds_CC_temp = (Time_CC[-1]-Time_CC[0]).seconds
 
